edd9a36f3a3a8d635ac7174bd6da8abc03001ffa
Rubendvb <rubenvasquez556@gmail.com>
2024-09-16 20:19:48 -0300

    creating database connection

---
+++ src/db/seed.ts
@@ -0,0 +1,37 @@
+import { client, db } from '.'
+import { goalCompletions, goals } from './schema'
+import dayjs from 'dayjs'
+
+async function seed() {
+  await db.delete(goalCompletions)
+  await db.delete(goals)
+
+  const result = await db
+    .insert(goals)
+    .values([
+      {
+        title: 'Learn Drizzle',
+        desiredWeeklyFrequency: 1,
+      },
+      {
+        title: 'Learn Js',
+        desiredWeeklyFrequency: 5,
+      },
+      {
+        title: 'Learn React',
+        desiredWeeklyFrequency: 6,
+      },
+    ])
+    .returning()
+
+  const startOfWeek = dayjs().startOf('week')
+
+  await db.insert(goalCompletions).values([
+    { goalId: result[0].id, createdAt: startOfWeek.toDate() },
+    { goalId: result[1].id, createdAt: startOfWeek.add(1, 'day').toDate() },
+  ])
+}
+
+seed().finally(() => {
+  client.end()
+})
